4bde34c0dd7df5bdb61c08e1110954ea1792d059
yKazihara <run5150@gmail.com>
2024-11-06 22:04:48 +0900

    文字リテラルと文字列リテラル。

---
+++ src/com/example/CharacterLiteralsSample.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class CharacterLiteralsSample {
+    public static void main(String[] args) {
+        System.out.println('a');
+        System.out.println('あ');
+
+        System.out.println(0x0061); // 97
+        System.out.println('\u0061'); // a
+    }
+}

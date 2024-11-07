0585f83ed9babe989cd53248aed10b96f491e304
yKazihara <run5150@gmail.com>
2024-11-07 21:45:30 +0900

    エスケープシーケンスのサンプル。

---
+++ src/com/example/EscapeSequencesSample.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class EscapeSequencesSample {
+    public static void main(String[] args) {
+        System.out.println('\''); // '
+        System.out.println("私の名前は\"鈴木\"です"); // 私の名前は"鈴木"です
+        System.out.println("この魚の値段は\\250です"); // この魚の値段は\250です
+    }
+}

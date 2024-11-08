d427383193062420ea6214f3bdc20b768f21a54b
yKazihara <run5150@gmail.com>
2024-11-08 21:56:31 +0900

    整数・浮動小数点数リテラルのサンプル。

---
+++ src/com/example/NumberLiteralsSample.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class NumberLiteralsSample {
+    public static void main(String[] args) {
+        System.out.println(123); // 123
+        System.out.println(0x3F01); // 16129
+        System.out.println(45 + 75); // 120
+
+        System.out.println("-----");
+
+        System.out.println(18.225); // 18.225
+        System.out.println(5.23e3); // 5230.0
+        System.out.println(1.816e10); // 1.816E10
+        System.out.println(7.5e-2); // 0.075
+        System.out.println(2.712e-6); // 2.712E-6
+        System.out.println(3.52 + 17.4); // 20.919999999999998
+    }
+}

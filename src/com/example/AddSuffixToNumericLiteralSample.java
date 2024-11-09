d4614559d29a1082c1511d4fdde9671a2c841db0
yKazihara <run5150@gmail.com>
2024-11-09 22:04:19 +0900

    数値リテラルにサフィックスを付けて型を指定するサンプル。

---
+++ src/com/example/AddSuffixToNumericLiteralSample.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class AddSuffixToNumericLiteralSample {
+    public static void main(String[] args) {
+        System.out.println(2200000000L);  // 2200000000 long
+        System.out.println(3.2);  // 3.2 double
+        System.out.println(3.2D);  // 3.2 double
+        System.out.println(3.2F);  // 3.2 float
+    }
+}

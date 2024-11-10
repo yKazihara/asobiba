731528b6377374c415f8df14f5c55a13b750edfc
yKazihara <run5150@gmail.com>
2024-11-10 21:40:34 +0900

    論理値リテラルのサンプル。

---
+++ src/com/example/BooleanLiteralsSample.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class BooleanLiteralsSample {
+    public static void main(String[] args) {
+        System.out.println(10 > 7); // true
+        System.out.println(5 == 4); // false
+    }
+}

0f60409374d64a0afdb55ef900cfdafa6089dbde
yKazihara <run5150@gmail.com>
2024-11-16 19:29:10 +0900

    単項マイナス演算子のサンプル。

---
+++ src/com/example/UnaryMinusOperatorSample.java
@@ -0,0 +1,10 @@
+package com.example;
+
+public class UnaryMinusOperatorSample {
+    public static void main(String[] args) {
+        int num = 10;
+
+        System.out.println(+num); // 10 当たり前か。しかしプラスつけられるって知らなかった。
+        System.out.println(-num); // -10 知らなかったかも。
+    }
+}

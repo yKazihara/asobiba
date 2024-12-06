b24a39e59aec6b24e2f6d1cf8b4ef61193abee74
yKazihara <run5150@gmail.com>
2024-12-06 22:08:39 +0900

    多次元配列の長さを取得するサンプルを追記。

--- src/com/example/InitializingMultidimensionalArraysSample.java
+++ src/com/example/InitializingMultidimensionalArraysSample.java
@@ -16,5 +16,29 @@ public class InitializingMultidimensionalArraysSample {
         //num1[1][0] = 76
         //num1[1][1] = 92
         //num1[1][2] = 48
+
+        System.out.println("-----");
+
+        // 多次元配列の長さを取得する
+        System.out.println(num1.length); // 2
+        System.out.println(num1[0].length); // 3
+        System.out.println(num1[1].length); // 3
+
+        System.out.println("-----");
+
+        int[][] num2 = {{87,54}, {76,92,48,58,84}};
+
+        for (int i = 0; i < num2.length; i++){
+            for (int j = 0; j < num2[i].length; j++){
+                System.out.println(STR."num2[\{i}][\{j}] = \{num2[i][j]}");
+            }
+        }
+        //num2[0][0] = 87
+        //num2[0][1] = 54
+        //num2[1][0] = 76
+        //num2[1][1] = 92
+        //num2[1][2] = 48
+        //num2[1][3] = 58
+        //num2[1][4] = 84
     }
 }

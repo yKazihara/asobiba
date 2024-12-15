c0229d22166bd8bcd364c8df1a7d6329baf2eb71
yKazihara <run5150@gmail.com>
2024-12-15 20:09:04 +0900

    Stringサンプルに追記。

--- src/com/example/StringLiteralsSample.java
+++ src/com/example/StringLiteralsSample.java
@@ -4,5 +4,22 @@ public class StringLiteralsSample {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("こんにちは");
+
+        System.out.println("-----");
+
+        String msg1 = "Hello";
+
+        char[] c = {'B', 'y', 'e'};
+        String msg2 = new String(c);
+
+        String msg3 = new String(msg1);
+
+        System.out.println(msg1); // Hello
+        System.out.println(msg2); // Bye
+        System.out.println(msg3); // Hello
+
+        msg2 = "Bye!";
+
+        System.out.println(msg2); // Bye!
     }
 }

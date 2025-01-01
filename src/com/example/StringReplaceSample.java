ca0b1160201470f6662a2abca8bea95933aabd47
yKazihara <run5150@gmail.com>
2025-01-01 22:34:24 +0900

    文字列の中の正規表現パターンとマッチする部分を置換するサンプルを追記。

--- src/com/example/StringReplaceSample.java
+++ src/com/example/StringReplaceSample.java
@@ -11,5 +11,16 @@ public class StringReplaceSample {
         String str2= "東京都港区";
         System.out.println(str2); // 東京都港区
         System.out.println(str2.replace("港区", "中央区")); // 東京都中央区
+
+        System.out.println("-----");
+
+        String regex  = "aA|Aa|aa";
+        String str = "AA BB aA Aa CC aa";
+
+        System.out.println(str.replaceFirst(regex, "AA"));
+        // AA BB AA Aa CC aa 最初にヒットしたaAのみ置換
+
+        System.out.println(str.replaceAll(regex, "AA"));
+        // AA BB AA AA CC AA　ヒットしたものは全部置換
     }
 }

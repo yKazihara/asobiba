9eb54aeff97db2db7ff082a3bec0ad479d63b2ec
yKazihara <run5150@gmail.com>
2025-01-31 17:21:26 +0900

    ファイルの最終更新日時の取得と設定のサンプルを修正。

--- src/com/example/FilesGetLastModifiedTimeSample.java
+++ src/com/example/FilesGetLastModifiedTimeSample.java
@@ -19,23 +19,21 @@ public class FilesGetLastModifiedTimeSample {
             LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
 
             System.out.println(localDateTime);
-            // 例：2025-01-31T16:19:10.213994217
+            // 例：2025-01-31T17:20:08.287500542
         } catch(IOException e) {
             System.out.println(e);
         }
 
         System.out.println("-----");
 
-        Path p2 = Paths.get("src/com/example/FilesGetLastModifiedTimeSample.java");
-
         try {
             FileTime fileTime = FileTime.from(Instant.now());
-            Files.setLastModifiedTime(p2, fileTime);
+            Files.setLastModifiedTime(p1, fileTime); // 最終更新日時を更新
 
             Instant instant = fileTime.toInstant();
             LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.systemDefault());
             System.out.println(localDateTime);
-            // 例：2025-01-31T16:22:38.773121
+            // 例：2025-01-31T17:20:10.145510
         } catch(IOException e) {
             System.out.println(e);
         }

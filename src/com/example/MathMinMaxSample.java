7c54017f8025f9ff3ba1f221964d6320d2156292
yKazihara <run5150@gmail.com>
2025-02-07 09:26:48 +0900

    どちらか大きい値または小さい値を取得するサンプル。

---
+++ src/com/example/MathMinMaxSample.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class MathMinMaxSample {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 12;
+
+        System.out.println(Math.max(num1, num2)); // 20
+        System.out.println(Math.min(num1, num2)); // 12
+    }
+}

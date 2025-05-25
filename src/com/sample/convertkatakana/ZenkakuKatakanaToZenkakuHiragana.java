0efaf9d298880b8fb3e63b0c5a0b3f4c7bae08bd
yKazihara <run5150@gmail.com>
2025-05-26 06:56:23 +0900

    全角カタカナを全角ひらがなへ変換するサンプル。

---
+++ src/com/sample/convertkatakana/ZenkakuKatakanaToZenkakuHiragana.java
@@ -0,0 +1,35 @@
+package com.sample.convertkatakana;
+
+public class ZenkakuKatakanaToZenkakuHiragana {
+    /*
+     * ぁあぃいぅうぇえぉおかがきぎくぐけげこごさざしじすずせぜそぞ
+     * ただちぢっつづてでとどなにぬねのはばぱひびぴふぶぷへべぺほぼぽ
+     * まみむめもゃやゅゆょよらりるれろゎわゐゑをん
+     *
+     * ァアィイゥウェエォオカガキギクグケゲコゴサザシジスズセゼソゾ
+     * タダチヂッツヅテデトドナニヌネノハバパヒビピフブプヘベペホボポ
+     * マミムメモャヤュユョヨラリルレロヮワヰヱヲンヴヵヶ
+     */
+    public static String zenkakuHiraganaToZenkakuKatakana(String s) {
+        StringBuffer sb = new StringBuffer(s);
+        for (int i = 0; i < sb.length(); i++) {
+            char c = sb.charAt(i);
+            if (c >= 'ァ' && c <= 'ン') {
+                sb.setCharAt(i, (char)(c - 'ァ' + 'ぁ'));
+            } else if (c == 'ヵ') {
+                sb.setCharAt(i, 'か');
+            } else if (c == 'ヶ') {
+                sb.setCharAt(i, 'け');
+            } else if (c == 'ヴ') {
+                sb.setCharAt(i, 'う');
+                sb.insert(i + 1, '゛');
+                i++;
+            }
+        }
+        return sb.toString();
+    }
+    public static void main(String[] args) {
+        System.out.println(zenkakuHiraganaToZenkakuKatakana("ジャヴァ・プログラミング"));
+        // じゃう゛ぁ・ぷろぐらみんぐ
+    }
+}

16a67f1da40a21d63879406cada76eff8847f510
yKazihara <run5150@gmail.com>
2025-05-28 07:02:15 +0900

    文字コードを文字に変換するサンプル。

---
+++ src/com/sample/encode/ConvertString.java
@@ -0,0 +1,13 @@
+package com.sample.encode;
+
+public class ConvertString {
+    public static void main(String[] args) {
+
+        // 「亜」の文字コードです。
+        char c = '\u4e9c';
+
+        // 文字コードに対応する文字を表示します。
+        System.out.println(c);
+        // 亜
+    }
+}

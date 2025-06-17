d6837372e87b88d5bb537a2d14300f2499c83f9d
yKazihara <run5150@gmail.com>
2025-06-18 07:06:34 +0900

    変数を宣言し、値を代入するサンプル。

---
+++ src/com/sample/variable/Variable.java
@@ -0,0 +1,26 @@
+package com.sample.variable;
+
+public class Variable {
+    public static void main(String[] args) {
+
+        //タイトル
+        String title;
+        title = "Java超入門";
+
+        //価格
+        int price;
+        price = 3000;
+
+        System.out.print("本のタイトルは");
+        System.out.print(title);
+        System.out.println("です。");
+        // 本のタイトルはJava超入門です。
+
+        System.out.println();
+
+        System.out.print("本の価格は");
+        System.out.print(price);
+        System.out.println("円です。");
+        // 本の価格は3000円です。
+    }
+}

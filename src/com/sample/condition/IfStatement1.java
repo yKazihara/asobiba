a83cc69d20de7bfc8c40d023bac11fce3eb7f769
yKazihara <run5150@gmail.com>
2025-06-30 06:58:00 +0900

    if文の条件式が成立するサンプル。

---
+++ src/com/sample/condition/IfStatement1.java
@@ -0,0 +1,21 @@
+package com.sample.condition;
+
+public class IfStatement1 {
+    public static void main(String[] args) {
+
+        // 比較用の値を管理する変数の宣言と初期化
+        int a = 5;
+        int b = 5;
+
+        System.out.println("-- if文開始 --");
+
+        if (a == b) {
+            System.out.println("変数aと変数bは等しい。");
+        }
+
+        System.out.println("-- if文終了 --");
+        //-- if文開始 --
+        //変数aと変数bは等しい。
+        //-- if文終了 --
+    }
+}

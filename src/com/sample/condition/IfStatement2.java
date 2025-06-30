50d66f4d98ec6c181ed380ea78925f15958889f3
yKazihara <run5150@gmail.com>
2025-07-01 06:53:31 +0900

    if文の条件式が成立しないサンプル。

---
+++ src/com/sample/condition/IfStatement2.java
@@ -0,0 +1,20 @@
+package com.sample.condition;
+
+public class IfStatement2 {
+    public static void main(String[] args) {
+
+        // 比較用の値を管理する変数の宣言と初期化
+        int a = 4;
+        int b = 5;
+
+        System.out.println("-- if文開始 --");
+
+        if (a == b) {
+            System.out.println("変数aと変数bは等しい。");
+        }
+
+        System.out.println("-- if文終了 --");
+        //-- if文開始 --
+        //-- if文終了 --
+    }
+}

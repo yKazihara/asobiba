40b53d3d3d182288a16afc434bdfa4a3bb2fdcf2
yKazihara <run5150@gmail.com>
2025-07-02 07:00:21 +0900

    !=演算子を用いた条件式が成立するサンプル。

---
+++ src/com/sample/condition/IfStatement3.java
@@ -0,0 +1,21 @@
+package com.sample.condition;
+
+public class IfStatement3 {
+    public static void main(String[] args) {
+
+        // 比較用の値を管理する変数の宣言と初期化
+        int a = 2;
+        int b = 5;
+
+        System.out.println("-- if文開始 --");
+
+        if (a != b) {
+            System.out.println("変数aと変数bは等しくない。");
+        }
+
+        System.out.println("-- if文終了 --");
+        //-- if文開始 --
+        //変数aと変数bは等しくない。
+        //-- if文終了 --
+    }
+}

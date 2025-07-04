95ed5c7ca2eaeb89e16696e9fd7ec0f3835e726f
yKazihara <run5150@gmail.com>
2025-07-05 07:19:16 +0900

    ネストしたif文内の条件が全て成立するサンプル。

---
+++ src/com/sample/condition/NestedIfStatement1.java
@@ -0,0 +1,26 @@
+package com.sample.condition;
+
+public class NestedIfStatement1 {
+    public static void main(String[] args) {
+
+        //比較用の数値を管理する為の変数aを宣言し、5で初期化
+        int a = 7;
+
+        System.out.println("*****if文の判定開始*****");
+
+        if(a >= 0){
+            System.out.println("変数aは0以上。");
+            if(a <= 10){
+                System.out.println("変数aは10以下。");
+            }else{
+                System.out.println("変数aは10より大きい。");
+            }
+        }
+
+        System.out.println("*****if文の判定終了*****");
+        //*****if文の判定開始*****
+        //変数aは0以上。
+        //変数aは10以下。
+        //*****if文の判定終了*****
+    }
+}

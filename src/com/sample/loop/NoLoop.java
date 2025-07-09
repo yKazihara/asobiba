b24f10b6b6de0b3229b1b2435c19152cfaf95bc5
yKazihara <run5150@gmail.com>
2025-07-10 07:07:07 +0900

    for文を使用せず同一処理を繰り返すサンプル。

---
+++ src/com/sample/loop/NoLoop.java
@@ -0,0 +1,13 @@
+package com.sample.loop;
+
+public class NoLoop {
+    public static void main(String[] args) {
+
+        System.out.println("超入門Loop!");
+        System.out.println("超入門Loop!");
+        System.out.println("超入門Loop!");
+        //超入門Loop!
+        //超入門Loop!
+        //超入門Loop!
+    }
+}

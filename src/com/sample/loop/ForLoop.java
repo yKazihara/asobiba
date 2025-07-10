115faabbf81f544382c01201074eaed6262130e9
yKazihara <run5150@gmail.com>
2025-07-11 06:50:29 +0900

    for文を使用し同一処理を繰り返すサンプル。

---
+++ src/com/sample/loop/ForLoop.java
@@ -0,0 +1,15 @@
+package com.sample.loop;
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 3; i++) {
+            System.out.println("超入門Loop!");
+        }
+        //超入門Loop!
+        //超入門Loop!
+        //超入門Loop!
+    }
+
+}

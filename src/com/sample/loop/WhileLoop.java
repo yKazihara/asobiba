3e4345b4caaf03d5fc67ad11ca9c968eaf7c0f9b
yKazihara <run5150@gmail.com>
2025-07-12 06:46:54 +0900

    while文を用いて同じメッセージを繰り返すサンプル。

---
+++ src/com/sample/loop/WhileLoop.java
@@ -0,0 +1,21 @@
+package com.sample.loop;
+
+public class WhileLoop {
+
+    public static void main(String[] args) {
+
+        // while文を制御する為の変数iを宣言
+        int i = 0;
+
+        while (i <= 4) {
+            System.out.println("超入門WhileLoop!");
+            i++;
+        }
+        //超入門WhileLoop!
+        //超入門WhileLoop!
+        //超入門WhileLoop!
+        //超入門WhileLoop!
+        //超入門WhileLoop!
+    }
+
+}

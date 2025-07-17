f877f9d3d3f4b645a7d3921a93208ceadd21a278
yKazihara <run5150@gmail.com>
2025-07-18 06:53:14 +0900

    ゲームタイトルと終了メッセージを表示するサンプル。

---
+++ src/com/sample/highandlow/HighAndLowS01.java
@@ -0,0 +1,19 @@
+package com.sample.highandlow;
+
+public class HighAndLowS01 {
+    public static void main(String[] args) {
+
+        System.out.println("**************");
+        System.out.println("* High & Low *");
+        System.out.println("**************");
+
+        System.out.println("");
+
+        System.out.println("--ゲーム終了--");
+        //**************
+        //* High & Low *
+        //**************
+        //
+        //--ゲーム終了--
+    }
+}

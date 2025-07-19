5fc380eb42e7545220563bf6fe70b590956b6ffc
yKazihara <run5150@gmail.com>
2025-07-20 07:48:31 +0900

    ランダムな数値をトランプの枠に合わせて表示するサンプル。

---
+++ src/com/sample/highandlow/HighAndLowS03.java
@@ -0,0 +1,37 @@
+package com.sample.highandlow;
+
+public class HighAndLowS03 {
+    public static void main(String[] args) {
+
+        int leftCard = (int)(Math.random()*9)+1;
+
+        System.out.println("**************");
+        System.out.println("* High & Low *");
+        System.out.println("**************");
+
+        System.out.println();
+
+        System.out.println("  [問題表示]");
+        System.out.println("*****    *****");
+        System.out.println("*   *    * * *");
+        System.out.println(STR."* \{leftCard} *    * * *");
+        System.out.println("*   *    * * *");
+        System.out.println("*****    *****");
+
+        System.out.println();
+
+        System.out.println("--ゲーム終了--");
+        //**************
+        //* High & Low *
+        //**************
+        //
+        //  [問題表示]
+        //*****    *****
+        //*   *    * * *
+        //* 7 *    * * *
+        //*   *    * * *
+        //*****    *****
+        //
+        //--ゲーム終了--
+    }
+}

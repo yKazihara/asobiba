d9b5864b0ae7c18356ac33502c4131555b068713
yKazihara <run5150@gmail.com>
2025-07-22 06:56:12 +0900

    隠れた数値を画面に表示するサンプル。

---
+++ src/com/sample/highandlow/HighAndLowS05.java
@@ -0,0 +1,66 @@
+package com.sample.highandlow;
+
+import java.util.Scanner;
+public class HighAndLowS05 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int leftCard = (int)(Math.random()*9)+1;
+        int rightCard = (int)(Math.random()*9)+1;
+
+        System.out.println("**************");
+        System.out.println("* High & Low *");
+        System.out.println("**************");
+
+        System.out.println();
+
+        System.out.println("  [問題表示]");
+        System.out.println("*****    *****");
+        System.out.println("*   *    * * *");
+        System.out.println(STR."* \{leftCard} *    * * *");
+        System.out.println("*   *    * * *");
+        System.out.println("*****    *****");
+        System.out.print(" High  or  Low ?(h/l)＞");
+        String select = sc.nextLine();
+
+        if(select.equals("h")) {
+            System.out.println(" →Highを選択しました。");
+        } else {
+            System.out.println(" →Lowを選択しました。");
+        }
+
+        System.out.println();
+
+        System.out.println("  [結果表示]");
+        System.out.println("*****    *****");
+        System.out.println("*   *    *   *");
+        System.out.println(STR."* \{leftCard} *    * \{rightCard} *");
+        System.out.println("*   *    *   *");
+        System.out.println("*****    *****");
+
+        System.out.println();
+
+        System.out.println("--ゲーム終了--");
+        //**************
+        //* High & Low *
+        //**************
+        //
+        //  [問題表示]
+        //*****    *****
+        //*   *    * * *
+        //* 7 *    * * *
+        //*   *    * * *
+        //*****    *****
+        // High  or  Low ?(h/l)＞l
+        // →Lowを選択しました。
+        //
+        //  [結果表示]
+        //*****    *****
+        //*   *    *   *
+        //* 7 *    * 6 *
+        //*   *    *   *
+        //*****    *****
+        //
+        //--ゲーム終了--
+    }
+}
